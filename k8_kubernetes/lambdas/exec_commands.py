5b48dc75730a71f9fe5855574f4d3bdc745bb8ed
Dinis Cruz <dinis.cruz@owasp.org>
2021-01-27 01:15:58 +0000

    improved exec_commands

--- k8_kubernetes/lambdas/exec_commands.py
+++ k8_kubernetes/lambdas/exec_commands.py
@@ -1,2 +1,5 @@
+from osbot_utils.utils.Process import exec_process
 def run(event, context=None):
-    return 'Going to execute some commands inside lambda'
+    command = event.get('command')
+    params  = event.get('params')
+    return exec_process(command, params)
--- tests/unit/lambdas/test_exec_commands.py
+++ tests/unit/lambdas/test_exec_commands.py
@@ -1,5 +1,6 @@
 from pprint                              import pprint
 from unittest                            import TestCase
+from osbot_utils.utils.Misc              import trim
 from osbot_aws.apis.Lambda               import Lambda
 from osbot_aws.deploy.Deploy_Lambda      import Deploy_Lambda
 from k8_kubernetes.lambdas.exec_commands import run
@@ -11,11 +12,18 @@ class test_hello_world(TestCase):
         self.aws_lambda    = Lambda(self.deploy_lambda.lambda_name())   # use OSBot_Lambda helper class to invoke lambdas
         print()
 
-    def _deploy_lambda(self):
+    def deploy_lambda(self):
+        self.deploy_lambda.add_osbot_utils()                            # adding the osbot_utils package to the source code
         self.deploy_lambda.deploy()                                     # create or update lambda function
 
+    def exec_in_lambda(self, command, params=None):
+        event = {'command': command, 'params' : params }
+        result = self.aws_lambda.invoke(event)
+        return trim(result.get('stdout')) + trim(result.get('stderr'))
+
     def test_invoke_lambda(self):
-        self._deploy_lambda()                                           # use when making changes to the lambda code
-        event = {}                                                      # payload to send to lambda function
-        result = self.aws_lambda.invoke(event)                          # invoke function in AWS
-        pprint(result)                                                  # print value returned by the function
+        result = self.exec_in_lambda('pwd')
+        pprint(result)
+
+    def test_pwd(self):
+        assert self.exec_in_lambda('pwd') == '/var/task'
