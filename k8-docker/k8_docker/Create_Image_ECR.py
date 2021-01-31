48a2c8e26ecc931d0c982d1df42790b5174197b7
Dinis Cruz <dinis.cruz@owasp.org>
2021-01-31 04:31:41 +0000

    first working version of Create_Image_ECR

--- k8-docker/k8_docker/Create_Image_ECR.py
+++ k8-docker/k8_docker/Create_Image_ECR.py
@@ -9,20 +9,27 @@ from osbot_utils.utils.Misc import base64_to_str
 
 class Create_Image_ECR:
 
-    def __init__(self, ecr_repository, image_name, image_tag='latest'):
+    def __init__(self, image_name, image_tag='latest'):
         self.api_docker      = API_Docker()
         self.ecr             = ECR()
         self.aws_config      = AWS_Config()
-        self.ecr_repository  = ecr_repository
         self.image_name      = image_name
         self.image_tag       = image_tag
 
     def build_image(self):
-        repository = self.image_name
+        repository = self.image_repository()
         tag        = self.image_tag
         result     = self.api_docker.image_build(path=self.path_image(), repository=repository, tag=tag)
         return result.get('status') == 'ok'
 
+    def create_repository(self):
+        self.ecr.repository_create(self.image_name)
+        return self.ecr.repository_exists(self.image_name)
+
+    def image_repository(self):
+        account_id = self.aws_config.aws_session_account_id()
+        region     = self.aws_config.aws_session_region_name()
+        return f'{account_id}.dkr.ecr.{region}.amazonaws.com/{self.image_name}'
 
     def ecr_login(self):
         auth_data = self.ecr.authorization_token()
@@ -30,9 +37,23 @@ class Create_Image_ECR:
                                               username=auth_data.get('username'),
                                               password=auth_data.get('password'))
 
+
     def path_image(self):
         return path_combine(self.path_images(), self.image_name)
 
     def path_images(self):
         return path_combine(__file__, '../../images')
 
+    def push_image(self):
+        return self.api_docker.image_push(self.image_repository(), self.image_tag)
+
+
+    def run(self):
+        create_repository = self.create_repository   ()
+        ecr_login         = self.ecr_login           ()
+        build_image       = self.build_image         ()
+        push_image        = self.push_image         ()
+        return {'create_repository' : create_repository,
+                'ecr_login'         : ecr_login        ,
+                'build_image'       : build_image      ,
+                'push_image'        : push_image       }
--- k8-docker/tests/unit/test_Create_Image_ECR.py
+++ k8-docker/tests/unit/test_Create_Image_ECR.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
 
+from osbot_utils.utils.Json import json_loads, json_parse
+
+from osbot_aws.AWS_Config import AWS_Config
+
 from osbot_utils.utils.Dev import pprint
 from osbot_utils.utils.Files import folder_exists, file_exists, path_combine
 
@@ -9,12 +13,21 @@ from k8_docker.Create_Image_ECR import Create_Image_ECR
 class test_Create_Image_ECR(TestCase):
 
     def setUp(self) -> None:
-        self.ecr_repository = 'osbot_unit_tests'
+        #self.ecr_repository = 'osbot_unit_tests'
         self.image_name     = 'scratch'
-        self._ = Create_Image_ECR(self.ecr_repository, self.image_name)
+        self._ = Create_Image_ECR(self.image_name)
 
     def test_build_image(self):
         assert self._.build_image() is True
+
+    def test_create_repository(self):
+        assert self._.create_repository() is True
+
+    def test_image_repository(self):
+        aws_config = AWS_Config()
+        account_id = aws_config.aws_session_account_id()
+        region     = aws_config.aws_session_region_name()
+        assert self._.image_repository() == f'{account_id}.dkr.ecr.{region}.amazonaws.com/{self.image_name}'
 
     def test_ecr_login(self):
         assert self._.ecr_login() == {'IdentityToken': '', 'Status': 'Login Succeeded'}
@@ -24,3 +37,13 @@ class test_Create_Image_ECR(TestCase):
 
     def test_path_images(self):
         assert folder_exists(self._.path_images())
+
+    def test_push_image(self):
+        result = self._.push_image()
+
+        pprint(result)
+
+
+    def test_run(self):
+        result = Create_Image_ECR('ubuntu').run()
+        pprint(result)
